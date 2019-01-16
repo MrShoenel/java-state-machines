2bbd730e3cf56fe0120667e0b6c40f98eac2a4b4
Sebastian Hönel <sehoaa@lnu.se>
2019-01-16 17:07:15 +0100

    Introduce concept of belonging state-machine to BaseState and add new constructor and getter/setter for the belonging machine

--- src/main/java/io/github/mrshoenel/stateMachines/state/BaseState.java
+++ src/main/java/io/github/mrshoenel/stateMachines/state/BaseState.java
@@ -1,8 +1,12 @@
 package io.github.mrshoenel.stateMachines.state;
 
+import io.github.mrshoenel.stateMachines.exception.NoSuchStateException;
 import io.github.mrshoenel.stateMachines.exception.NoSuchTransitionException;
+import io.github.mrshoenel.stateMachines.stateMachine.BaseStateMachine;
+import io.github.mrshoenel.stateMachines.stateMachine.StateMachine;
 import io.github.mrshoenel.stateMachines.transition.Transition;
 import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
 
 import java.util.Collections;
 import java.util.HashMap;
@@ -24,17 +28,64 @@ public class BaseState implements State {
 
     private final Map<String, Transition> transitionsUnmod;
 
+    protected BaseStateMachine belongsToMachine;
+
     /**
      * Initializes a new instance of a {@link BaseState}.
      *
      * @param name A name that should uniquely identify this state across
      *             the entire machine it is part of.
+     * @param machine A {@link BaseStateMachine} this state belongs to. This
+     *                machine can be null and also be assigned later. States
+     *                that belong to a machine will make it aware if there is
+     *                a transition activating them.
      */
-    public BaseState(@NonNull final String name) {
+    public BaseState(@NonNull final String name, @Nullable final BaseStateMachine machine) {
         this.name = Objects.requireNonNull(name);
+        this.setBelongsToMachine(machine);
         this.transitions = new HashMap<>();
         this.transitionsUnmod = Collections.unmodifiableMap(this.transitions);
         this.initialize();
+    }
+
+    /**
+     * Calls {@link BaseState#BaseState(String, BaseStateMachine)} with the
+     * argument for the machine being null (i.e. this state will not belong to
+     * a machine initially).
+     *
+     * @param name A name that should uniquely identify this state across
+     *             the entire machine it is part of.
+     */
+    public BaseState(@NonNull final String name) {
+        this(name, null);
+    }
+
+    /**
+     * Returns the {@link BaseStateMachine} this state belongs to, i.e. it
+     * was defined on the machine using {@link BaseStateMachine#defineState(State)}.
+     * When a state is defined using {@link BaseStateMachine#defineState(State)},
+     * then that method will call this method to register the machine.
+     *
+     * @return {@link BaseStateMachine} or {@code null}, if this state does not
+     * belong to a machine (yet).
+     */
+    public BaseStateMachine getBelongsToMachine() {
+        return belongsToMachine;
+    }
+
+    /**
+     * Setter for the {@link BaseStateMachine} this state belongs to. Note that
+     * the machine is nullable and this can be used to unset the relation. If a
+     * state is being removed/undefined from a machine using the method
+     * {@link BaseStateMachine#undefineState(State)}, then that method will also
+     * call this method with an argument of null, to remove the association.
+     *
+     * @param stateMachine
+     * @return
+     */
+    public BaseState setBelongsToMachine(@Nullable final BaseStateMachine stateMachine) {
+        this.belongsToMachine = stateMachine;
+        return this;
     }
 
     /**
